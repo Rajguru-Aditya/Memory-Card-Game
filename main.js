40acf403cb96247623f29716d2328d80584216b3
Aditya Rajguru <rajguru.aditya.25@gmail.com>
2021-06-27 10:24:12 +0530

    New Commit

--- main.js
+++ main.js
@@ -1,81 +1,3 @@
-// const cards = document.querySelectorAll(".memory-card");
-
-// let cardFlipped = false;
-// let lockBoard = false; // We will lock the board to prevent any bugs occurring due to edge cases
-// let firstCard, secondCard;
-
-// function flipCard() {
-//   if (lockBoard) return;
-//   if (this === firstCard) return;
-
-//   this.classList.add("flip");
-
-//   if (!cardFlipped) {
-//     // First click
-//     cardFlipped = true;
-//     firstCard = this;
-
-//     return;
-//   }
-//   // Second click
-//   secondCard = this;
-
-//   checkForMatch();
-// }
-
-// // Check if the cards match
-// function checkForMatch() {
-//   // if (firstCard.dataset.framework === secondCard.dataset.framework) {
-//   //   // Its a match
-//   //   disableCards();
-//   // } else {
-//   //   // Not a match
-//   //   unFlipCards();
-//   // }
-
-//   // We will write the above if else block with ternary operator
-
-//   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-//   isMatch ? disableCards() : unFlipCards();
-//   // In ternary operator, the first thing is our condition "isMatch" and then "?" means "if" .. and  ":" means "else"
-//   // If it is a match then first function after "?" will be executed, else the function after ":" will be executed
-//   // If you want to try if else instead of ternary operator then remove the comments from above if else block and comment the ternary operator
-// }
-
-// function disableCards() {
-//   firstCard.removeEventListner("click", flipCard);
-//   secondCard.removeEventListner("click", flipCard);
-
-//   resetBoard();
-// }
-
-// function unFlipCards() {
-//   lockBoard = true;
-//   // This will automatically flip the cards back after 1.5 seconds
-//   setTimeout(() => {
-//     firstCard.classList.remove("flip");
-//     secondCard.classList.remove("flip");
-
-//     resetBoard();
-//   }, 1500);
-// }
-
-// // Resetting the board
-// function resetBoard() {
-//   [cardFlipped, lockBoard] = [false, false];
-//   [firstCard, secondCard] = [null, null];
-// }
-
-// (function shuffle() {
-//   cards.forEach((card) => {
-//     let randomPos = Math.floor(Math.random() * 12);
-//     card.style.order = randomPos;
-//   });
-// })(); // We wrap the entire function into parenthesis to make this function an "immediately invoked function"
-// // It will be called immediately after its definition
-
-// cards.forEach((card) => card.addEventListener("click", flipCard));
-
 const cards = document.querySelectorAll(".memory-card");
 
 let hasFlippedCard = false;
@@ -144,6 +66,7 @@ function resetBoard() {
   [firstCard, secondCard] = [null, null];
 }
 
+// Shuffling the cards so they appear at random positions
 (function shuffle() {
   cards.forEach((card) => {
     let randomPos = Math.floor(Math.random() * 12);
